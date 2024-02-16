65afd144bb1566971fc22ee3b2fa45729c1bf894
purushottam kumar <pks050505@gmail.com>
2024-02-17 00:00:12 +0530

    [03_calculator dart 3 switch operator can we user better ways

--- 01_basic_challenges_1/02_get_sum/get_sum_test.dart
+++ 01_basic_challenges_1/02_get_sum/get_sum_test.dart
@@ -2,11 +2,19 @@ import 'package:test/test.dart';
 
 import 'get_sum.dart';
 
-void main() {
-  test('Calculating the sum of two numbers', () {
-    const num1 = 5;
-    const num2 = 10.5;
-    final num result = getSum(num1, num2);
-    expect(result, 15.5);
+
+void main(){
+  test("calculate the sum of two numbers", () {
+    const num1= 10;
+    const num2=11;
+    final num result=getSum(num1, num2);
+    expect(result, 21);
+  });
+  test("calculate the number 5 and 6 is 11", () {
+    const num1=5;
+    const num2=6;
+    final num result=getSum(num1, num2);
+    expect(result, 11);
   });
 }
+
--- 01_basic_challenges_1/03_calculator/calculator_solution.dart
+++ 01_basic_challenges_1/03_calculator/calculator_solution.dart
@@ -1,16 +1,22 @@
 import 'utils/operators.dart';
 
 num calculator(num num1, num num2, Operation opearator) {
-  switch (opearator) {
-    case Operation.addition:
-      return num1 + num2;
-    case Operation.substraction:
-      return num1 - num2;
-    case Operation.multiplication:
-      return num1 * num2;
-    case Operation.division:
-      return num1 / num2;
-    default:
-      throw Exception('Invalid operator');
-  }
+  // switch (opearator) {
+  //   case Operation.addition:
+  //     return num1 + num2;
+  //   case Operation.substraction:
+  //     return num1 - num2;
+  //   case Operation.multiplication:
+  //     return num1 * num2;
+  //   case Operation.division:
+  //     return num1 / num2;
+  //   default:
+  //     throw Exception('Invalid operator');
+  // }
+  return switch (opearator) {
+    Operation.addition => num1 + num2,
+    Operation.substraction => num1 - num2,
+    Operation.multiplication => num1 * num2,
+    Operation.division => num1 / num2,
+  };
 }
